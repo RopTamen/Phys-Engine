8f9383b7e70bc0b97b8ab637ecaa7dca2853be91
RopTamen <connermoyer1@gmail.com>
2025-02-05 18:01:28 -0800

    working gl

--- Phys Engine/BaseLayer.cpp
+++ Phys Engine/BaseLayer.cpp
@@ -12,16 +12,16 @@
 #include <gl/GLU.h>
 #include <GLFW/glfw3.h>
 
-
 using namespace std;
 
 //environemnt variables, in meters
-array<float, 3> boundaries = { 100, 100, 100 };
+array<float, 3> boundaries = { 20, 20, 20 };
 float t = 0.006f;
 float g = 9.81f;
 int loopExit = 0;
 float glTime = float(glfwGetTime());
 float fM_PI = float(M_PI);
+int resx = 800, resy = 800;
 
 //origin point and velocity storage for generic physics object
 class PhysObj {
@@ -35,10 +35,11 @@ public:
 	PhysObj() {
 		pos = { 0,0,0 };
 		vel = { 0,0,0 };
-		r = 0;
-		verts = { 0.0f,  0.5f, 1.0f, 0.0f, 0.0f, 
-				 -0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
-				  0.5f, -0.5f, 0.0f, 0.0f, 1.0f };
+		r = 0.5f;
+		verts = { 0.1f,  0.1f, 1.0f, 0.0f, 0.0f, 
+				  0.1f, -0.1f, 0.0f, 1.0f, 0.0f,
+				 -0.1f, -0.1f, 0.0f, 0.0f, 1.0f,
+				 -0.1f,  0.1f, 1.0f, 1.0f, 1.0f };
 	};
 
 	PhysObj(array<float, 3> posInit, array<float, 3> velInit, float rInit) {
@@ -49,9 +50,15 @@ public:
 
 	void writePos(array<float, 3> posNew) {
 		pos = posNew;
-		verts = { pos[0]       , pos[2] + 0.5f, verts[2] , verts[3] , verts[4],
-				  pos[0] - 0.5f, pos[2] - 0.5f, verts[7] , verts[8] , verts[9],
-				  pos[0] + 0.5f, pos[2] - 0.5f, verts[12], verts[13], verts[14] };
+		//verts = { pos[0] / 20 + 0.1f, pos[2] / 20 + 0.1f, verts[2] , verts[3] , verts[4] ,
+		//		  pos[0] / 20 + 0.1f, pos[2] / 20 - 0.1f, verts[7] , verts[8] , verts[9] ,
+		//		  pos[0] / 20 - 0.1f, pos[2] / 20 - 0.1f, verts[12], verts[13], verts[14],
+		//		  pos[0] / 20 - 0.1f, pos[2] / 20 + 0.1f, verts[17], verts[18], verts[19] };
+
+		verts = { pos[0] + 0.1f, pos[2] + 0.1f, verts[2] , verts[3] , verts[4] ,
+				  pos[0] + 0.1f, pos[2] - 0.1f, verts[7] , verts[8] , verts[9] ,
+				  pos[0] - 0.1f, pos[2] - 0.1f, verts[12], verts[13], verts[14],
+				  pos[0] - 0.1f, pos[2] + 0.1f, verts[17], verts[18], verts[19] };
 	};
 
 	void writeVel(array<float, 3> velNew) {
@@ -86,28 +93,33 @@ public:
 vector<PhysObj> objects;
 
 //main sim
-void physicsSim(float timeDiff) {
+void physicsSim(float deltaT) {
 	for (int iter = 0; iter < objects.size(); iter++) {
 		array<float, 3> pos = objects[iter].getPos();
 		array<float, 3> vel = objects[iter].getVel();
 		float r = objects[iter].getr();
 
-		//if object moves past boundary, move it back and invert velocity, sits at -100 for 2 frames for some reason, not sure why
+		cout << deltaT << endl;
+
+		//if object were to move past boundary in next frame, don't update position, invert velocity
 		for (int coll = 0; coll < pos.size(); coll++) {
-			if (pos[coll] - r <= -boundaries[coll] || pos[coll] + r >= boundaries[coll]) {
+			if (pos[coll] - r + vel[coll] * deltaT <= -boundaries[coll] || pos[coll] + r + vel[coll] * deltaT >= boundaries[coll]) {
 				vel[coll] = -vel[coll];
-				pos[coll] = pos[coll] - (pos[coll] / abs(pos[coll])) * (abs(pos[coll]) - boundaries[coll]);
+				//pos[coll] = pos[coll] - (pos[coll] / abs(pos[coll])) * (abs(pos[coll]) - boundaries[coll]);
+			}
+			else {
+				pos[coll] = pos[coll] + vel[coll] * deltaT;
 			};
 		};
 
 		//update position based on velocity, then update velocity
-		pos = { pos[0] + vel[0] * timeDiff, pos[1] + vel[1] * timeDiff, pos[2] + vel[2] * timeDiff };
-		vel = { vel[0], vel[1], vel[2] - (g * timeDiff) };
+		//pos = { pos[0] + vel[0], pos[1] + vel[1], pos[2] + vel[2] };
+		vel = { vel[0], vel[1], (vel[2] - g * deltaT) };
 
 		objects[iter].writePos(pos);
 		objects[iter].writeVel(vel);
 
-		cout << "xpos: " << objects[iter].getPos()[0] << " " << "ypos: " << objects[iter].getPos()[1] << " " << "zpos: " << objects[iter].getPos()[2] << endl;
+		//cout << "xpos: " << objects[iter].getPos()[0] << " " << "ypos: " << objects[iter].getPos()[1] << " " << "zpos: " << objects[iter].getPos()[2] << endl;
 	};
 };
 
@@ -124,12 +136,13 @@ const GLchar* vertexSource = R"glsl(
 	in vec2 position;
 	in vec3 colorVert;
 
+	uniform vec2 positionSphere;
+
 	out vec3 colorFrag;
 
 	void main() {
 		vec2 positionNew;
-		positionNew.x = position.x;
-		positionNew.y = position.y * -1.0;
+		positionNew = position + positionSphere;
 		colorFrag = colorVert;
 		gl_Position = vec4(positionNew, 0.0, 1.0);
 	};
@@ -161,7 +174,7 @@ int main() {
 	if (!glfwInit()) { return 0; };
 
 	//init window object named window with title Sample
-	GLFWwindow* window = glfwCreateWindow(800, 800, "Sample", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(resx, resy, "Sample", NULL, NULL);
 	if (!window) { cout << "failed window creation" << endl; return 0; };
 
 	//set escape key callback and current context
@@ -173,15 +186,15 @@ int main() {
 	//init object and push to vector
 	PhysObj Sphere;
 	Sphere.writePos({ 0, 0, 0 });
-	Sphere.writeVel({ 0, 5, 15 });
+	Sphere.writeVel({ 5, 0, 20 });
 	Sphere.writer(0.5);
 
 	objects.insert(objects.begin(), Sphere);
 	vector<float> vertsVec = objects[0].getVerts();
-	vertsVec = {  0.5f,  0.5f, 1.0f, 0.0f, 0.0f,
-				  0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
-				 -0.5f, -0.5f, 0.0f, 0.0f, 1.0f,
-				 -0.5f,  0.5f, 0.5f, 0.5f, 0.5f };
+	//vertsVec = {  0.5f,  0.5f, 1.0f, 0.0f, 0.0f,
+	//			  0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+	//			 -0.5f, -0.5f, 0.0f, 0.0f, 1.0f,
+	//			 -0.5f,  0.5f, 0.5f, 0.5f, 0.5f };
 	float verts[20];
 	copy(vertsVec.begin(), vertsVec.end(), verts);
 
@@ -234,10 +247,11 @@ int main() {
 
 	//cout << glGetError() << endl;
 
-	//GLint uniColor = glGetUniformLocation(shaderProgram, "triangleColor");
+	GLint uniPos = glGetUniformLocation(shaderProgram, "positionSphere");
 
-	float time1 = 0, time2 = 0, timeDiff = 0;
+	float time1 = 0, time2 = 0, deltaT = 0;
 	int i= 0;
+	array<float, 2> positionNew;
 
 	//main loop
 	while (!glfwWindowShouldClose(window) && loopExit == 0) {
@@ -251,16 +265,16 @@ int main() {
 		};
 		i++;
 
-		timeDiff = abs(time2 - time1);
+		deltaT = abs(time2 - time1);
 
 		//render above
 		int width, height;
 		glfwGetFramebufferSize(window, &width, &height);
 		glViewport(0, 0, width, height);
 		glClear(GL_COLOR_BUFFER_BIT);
-		glDrawElements(GL_TRIANGLES, /*sizeof(elements) / sizeof(GLuint)*/ 3, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, sizeof(elements) / sizeof(GLuint), GL_UNSIGNED_INT, 0);
 
-		physicsSim(timeDiff);
+		physicsSim(deltaT);
 		//vertsVec = objects[0].getVerts();
 		//vertsVec = { vertsVec[0] , vertsVec[1] , (cos(glTime) + 1) / 2, (cos(glTime + (4 * fM_PI) / 3) + 1) / 2, (cos(glTime + (2 * fM_PI) / 3) + 1) / 2,
 		//		     vertsVec[5] , vertsVec[6] , (cos(glTime + (2 * fM_PI) / 3) + 1) / 2, (cos(glTime) + 1) / 2, (cos(glTime + (4 * fM_PI) / 3) + 1) / 2,
@@ -269,10 +283,15 @@ int main() {
 		//objects[0].writeVerts(vertsVec);
 
 		//Sleep(t * 1000.0f);
+
+		positionNew = { objects[0].getPos()[0], objects[0].getPos()[2] };
+		//cout << positionNew[0] << " " << positionNew[1] << endl;
+		glUniform2f(uniPos, positionNew[0]/boundaries[0], positionNew[1]/boundaries[2]);
+
 		glfwPollEvents();
 		glfwSwapBuffers(window);
 
-		cout << glGetError() << endl;
+		//cout << glGetError() << endl;
 	};
 
 	glfwDestroyWindow(window);
--- Phys Engine/Shaders.vert
+++ Phys Engine/Shaders.vert
@@ -3,12 +3,15 @@
 in vec2 position;
 in vec3 colorVert;
 
+uniform vec2 positionSphere;
+
 out vec3 colorFrag;
 
 void main() {
 	vec2 positionNew;
-	positionNew.x = position.x;
-	positionNew.y = position.y * -1.0;
+	positionNew = position + positionSphere;
+	//positionNew.x = position.x;
+	//positionNew.y = position.y * -1.0;
 	colorFrag = colorVert;
 	gl_Position = vec4(positionNew, 0.0, 1.0);
 };
